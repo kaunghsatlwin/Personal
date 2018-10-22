4833defa1cad090cf22506630a187e253e256250
Kaung Hsat Lwin <kaunghsatlwin@gmail.com>
2018-10-22 22:43:15 +0900

    license header added :wq

--- MyPortfolio/src/main/java/com/portfolio/controller/BlogController.java
+++ MyPortfolio/src/main/java/com/portfolio/controller/BlogController.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * copyright (c) 2018. All rights reserved.
  */
 package com.portfolio.controller;
 
